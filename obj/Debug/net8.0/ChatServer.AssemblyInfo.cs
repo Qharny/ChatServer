c018c15417419f9cb83b28db0b99e0d3eb01c877
qharny <kabuteymanasseh5@gmail.com>
2025-08-11 01:13:58 +0000

    features

--- obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73c27e5d18812cb97781a80e931c6fb930b56da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfdfb8230caf959b37095cf5688f875cfe7a978")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
